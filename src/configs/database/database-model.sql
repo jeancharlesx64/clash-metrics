28918259bcc0bbb0ec0a5b54de1713461ddf4db9
jeancharlesx64 <jean.albuquerque@sptech.school>
2024-05-23 12:01:44 -0300

    fixing the dashboard

--- src/configs/database/database-model.sql
+++ src/configs/database/database-model.sql
@@ -10,4 +10,10 @@ CREATE TABLE usuario(
     dataCriacao DATETIME
 );
 
+
+INSERT INTO usuario VALUES
+	(DEFAULT, 'ashigupta', 'ashi.gupta@prabhat.school', 'ashigupta123', '#RYQPCJGYY', NOW()),
+    (DEFAULT, 'jeancharlesx64', 'jean.albuquerque@sptech.school', 'jeancharles123', '#9PPYCYC28', NOW()),
+    (DEFAULT, 'teodorosamuel', 'samuel.teodoro@sptech.school', 'samuel123', '#99LLGL9VJ', NOW());
+    
 SELECT * FROM usuario;
