5b88155de7346c7c6ca0d6ea6ab2906fae7d5b57
jeancharlesx64 <jean.albuquerque@sptech.school>
2024-06-05 23:23:53 -0300

    new posts..

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 #9PPYCYC28 - Jean 
 #99LLGL9VJ - Samuel
 #J2RGRGR82 - Alisson
+#RJQCL9VYC - Fernanda
--- src/configs/database/database-model.sql
+++ src/configs/database/database-model.sql
@@ -37,7 +37,8 @@ ALTER TABLE post MODIFY descricao VARCHAR(150) CHARACTER SET utf8mb4 COLLATE utf
 
 INSERT INTO usuario VALUE
     (DEFAULT, 'jeancharlesx64', 'jean.albuquerque@sptech.school', 'jean123', '#9PPYCYC28','1717551406929_jean-profile.jfif', NOW()),
-	(DEFAULT, 'ashi007', 'ashi.gupta@outlook.com', 'ashi123', '#RYQPCJGYY','1717552089584_ashi-profile.jfif', NOW());
+	(DEFAULT, 'ashi007', 'ashi.gupta@outlook.com', 'ashi123', '#RYQPCJGYY','1717552089584_ashi-profile.jfif', NOW()),
+    (DEFAULT, 'dechamps', 'samuel.tsouza@sptech.school', 'samuel123', '#99LLGL9VJ','1717640238349_imagem_2024-06-05_231717437.png', NOW());
     
 INSERT INTO estatistica VALUE
 	(DEFAULT, NULL, NULL, NULL, NULL, 1),
@@ -45,9 +46,12 @@ INSERT INTO estatistica VALUE
 
 INSERT INTO post VALUE
 	(DEFAULT, 'Vocês conseguiram jogar neste desafio??', '1717436041633_ss-teste.jfif', '2024-06-03 14:34:01', 1),
-    (DEFAULT, 'Olhem esse deck que eu criei!! 😊', '1717634182771_ashi-deck.jfif', '2024-06-05 21:36:23', 2);
+    (DEFAULT, 'Olhem esse deck que eu criei!! 😊', '1717634182771_ashi-deck.jfif', '2024-06-05 21:36:23', 2),
+    (DEFAULT, 'Me expulsaram do clã 😡😡😡', '1717638577476_jean-clan.jfif', '2024-06-05 22:49:37', 1),
+    (DEFAULT, 'Sequência de vitórias 😎 samuel joga demais hehe', '1717639495730_jean-victory.jfif', '2024-06-05 23:04:56', 1);
     
 SELECT * FROM usuario;
 SELECT * FROM estatistica;
 SELECT * FROM post;
+
 
